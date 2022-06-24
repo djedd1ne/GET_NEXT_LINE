f0ecb068b4340eb26f800943d55d03452758c0be
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2022-06-24 06:36:31 +0200

    use the correct header for the bonus part

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
